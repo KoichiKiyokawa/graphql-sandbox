80b8dfb2c37dd2d5d4e39d4eea77000540fdcdac
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-03-07 22:50:46 +0900

    wip

--- nexus-realworld/apps/svelte-kit-apollo/src/plugins/apollo.ts
+++ nexus-realworld/apps/svelte-kit-apollo/src/plugins/apollo.ts
@@ -1,4 +1,5 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import * as pkg from '@apollo/client';
+const { ApolloClient, InMemoryCache } = pkg;
 
 const client = new ApolloClient({
 	cache: new InMemoryCache(),
