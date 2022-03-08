3e663f5956463e96cebd31d3031be41a1527180d
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-03-08 23:07:36 +0900

    chore: add issue comment

--- nexus-realworld/apps/svelte-kit-apollo/src/plugins/apollo.ts
+++ nexus-realworld/apps/svelte-kit-apollo/src/plugins/apollo.ts
@@ -1,3 +1,4 @@
+// https://github.com/apollographql/apollo-client/issues/8218#issuecomment-941256676
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 
 const client = new ApolloClient({
