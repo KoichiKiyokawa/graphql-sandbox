70cf66e42985d5b35fb0d12715e0a315f675231b
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-03-11 20:26:07 +0900

    fix: where in

--- go-gqlgen-sqlboiler/dataloader/loader.go
+++ go-gqlgen-sqlboiler/dataloader/loader.go
@@ -18,10 +18,20 @@ const key = "loader"
 
 func Middleware(db *sql.DB, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := context.WithValue(r.Context(), key, &loaders{
+		ctx := r.Context()
+		ctx = context.WithValue(ctx, key, &loaders{
 			UserById: UserLoader{
 				fetch: func(keys []string) ([]*models.User, []error) {
-					users, err := models.Users(qm.WhereIn("id in ?", keys)).All(r.Context(), db)
+					if len(keys) == 0 {
+						return nil, nil
+					}
+
+					var ids []interface{}
+					for _, key := range keys {
+						ids = append(ids, key)
+					}
+
+					users, err := models.Users(qm.WhereIn(models.UserColumns.ID+" in ?", ids...)).All(ctx, db)
 					return users, []error{err}
 				},
 			},
