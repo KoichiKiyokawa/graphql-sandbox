04172d43e5fa85eaa5744910f24367f9729bc452
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-06-19 12:57:54 +0900

    refactor: `AuthForm` story

--- twiter-clone/redwood-chakra/web/src/components/Auth/AuthForm/AuthForm.stories.tsx
+++ twiter-clone/redwood-chakra/web/src/components/Auth/AuthForm/AuthForm.stories.tsx
@@ -1,7 +1,12 @@
+import { ComponentStoryFn } from '@storybook/react'
+
 import AuthForm from './AuthForm'
 
-export const generated = () => {
-  return <AuthForm />
-}
+export const generated: ComponentStoryFn<typeof AuthForm> = (args) => (
+  <AuthForm {...args} />
+)
 
-export default { title: 'Components/AuthForm' }
+export default {
+  title: 'Components/AuthForm',
+  component: AuthForm,
+}
