7686ca2beade086d66b90dc14b13b24831439999
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-07-13 22:09:52 +0900

    feat: gracefull shutdown

--- go-gqlgen-sqlc/server.go
+++ go-gqlgen-sqlc/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"go-gqlgen-sqlc/db"
 	"go-gqlgen-sqlc/generated"
@@ -9,6 +10,9 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -30,11 +34,31 @@ func main() {
 	queries := db.New(database)
 	loaders := loader.NewLoaders(queries)
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolver.NewResolver(queries)}))
+	gqlSrv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: resolver.NewResolver(queries)}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", loader.Middleware(loaders, srv))
+	http.Handle("/query", loader.Middleware(loaders, gqlSrv))
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe("127.0.0.1:"+port, nil))
+	srv := &http.Server{Addr: "127.0.0.1:" + port}
+
+	// cf) https://github.com/gin-gonic/gin#manually
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so don't need to add it
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal(err)
+	}
 }
