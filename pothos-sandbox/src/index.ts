7c3ed62adda818dd19d87fa34a773efc49c111c6
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-08-29 22:22:45 +0900

    chore: tweek

--- pothos-sandbox/src/index.ts
+++ pothos-sandbox/src/index.ts
@@ -1,33 +1,8 @@
 import { createServer } from "@graphql-yoga/node";
-import SchemaBuilder from "@pothos/core";
-
-const builder = new SchemaBuilder({});
-
-class User {
-  id!: string;
-  name!: string;
-  email!: string;
-}
-
-builder.objectType(User, {
-  name: "User",
-  description: "User",
-  fields: () => ({}),
-});
-
-builder.queryType({
-  fields: (t) => ({
-    user: t.field({
-      type: User,
-      resolve() {
-        return new User();
-      },
-    }),
-  }),
-});
+import { schema } from "./domains/user/schema";
 
 const server = createServer({
-  schema: builder.toSchema({}),
+  schema,
 });
 
 server.start();
