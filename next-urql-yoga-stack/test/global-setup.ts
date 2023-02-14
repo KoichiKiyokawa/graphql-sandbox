6fa64ee78d6b29f0d24cd8252115062c6c8727e0
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-02-15 00:03:31 +0900

    ci

--- next-urql-yoga-stack/test/global-setup.ts
+++ next-urql-yoga-stack/test/global-setup.ts
@@ -2,5 +2,6 @@ import { execSync } from "child_process"
 
 export default function globalSetup(): void {
   process.env.DATABASE_URL = "file:./test.db"
+  process.env.TZ = "Asia/Tokyo"
   execSync("pnpm prisma db push")
 }
