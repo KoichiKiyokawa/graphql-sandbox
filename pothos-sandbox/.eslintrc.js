83db2e3aef44e34f9080fcc49160b908bd51c913
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-07-24 23:24:42 +0900

    chore: add eslint setting to ban builder.*Fields methods

--- pothos-sandbox/.eslintrc.js
+++ pothos-sandbox/.eslintrc.js
@@ -1,11 +1,29 @@
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
+  ignorePatterns: ['.eslintrc.js'],
   overrides: [
     {
       files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
       extends: ['standard-with-typescript', 'prettier'],
       parserOptions: {
         project: './tsconfig.json',
+      },
+      rules: {
+        'no-restricted-syntax': [
+          'error',
+          {
+            message: 'Use queryField instead of queryFields',
+            selector: 'MemberExpression > Identifier[name="queryFields"]',
+          },
+          {
+            message: 'Use mutationField instead of mutationFields',
+            selector: 'MemberExpression > Identifier[name="mutationFields"]',
+          },
+          {
+            message: 'Use subscriptionField instead of subscriptionFields',
+            selector: 'MemberExpression > Identifier[name="subscriptionFields"]',
+          },
+        ],
       },
     },
   ],
