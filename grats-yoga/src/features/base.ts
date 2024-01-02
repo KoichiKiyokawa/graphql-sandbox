f6d8f5170d7ef2ae35f2d5a65d4ca6af371ce010
Koichi Kiyokawa <koichi20110068@gmail.com>
2024-01-02 23:57:26 +0900

    chore: setup drizzle

--- grats-yoga/src/features/base.ts
+++ grats-yoga/src/features/base.ts
@@ -1,2 +1,8 @@
+import { db } from "../lib/db";
+
 /** @gqlType */
 export type Query = unknown;
+
+export type GqlContext = {
+  db: typeof db;
+};
--- grats-yoga/src/features/user.ts
+++ grats-yoga/src/features/user.ts
@@ -1,27 +1,31 @@
-import { type Query } from "./base";
+import { GqlContext, type Query } from "./base";
 
 /** @gqlType */
 type User = {
-	/** @gqlField */
-	id: string;
-	/** @gqlField */
-	name: string;
+  /** @gqlField */
+  id: string;
+  /** @gqlField */
+  name: string;
 };
 
 /** @gqlField */
-export async function user(_: Query, args: { id: string }): Promise<User> {
-	return {
-		id: args.id,
-		name: "hoge",
-	};
+export async function user(
+  _: Query,
+  args: { id: string },
+  ctx: GqlContext
+): Promise<User | null> {
+  const user = await ctx.db.query.users.findFirst({
+    where: (field, { eq }) => eq(field.id, args.id),
+  });
+  return user ?? null;
 }
 
 /** @gqlField */
-export async function users(_: Query): Promise<User[]> {
-	return [
-		{
-			id: "1",
-			name: "hoge",
-		},
-	];
+export async function users(
+  _: Query,
+  _args: unknown,
+  ctx: GqlContext
+): Promise<User[]> {
+  const users = await ctx.db.query.users.findMany();
+  return users;
 }
